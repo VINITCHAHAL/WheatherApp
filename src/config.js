b01f0c51864e62e5b5f54bcfcf8b1dfbb650ce9a
Vinit Choudhary <vinitchahal1241@gmail.com>
2025-07-01 00:10:33 +0530

    Updated to add mars wheather

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 const config = {
   WEATHER_API_BASE_URL: "https://api.openweathermap.org/data/2.5",
   WEATHER_API_KEY: process.env.REACT_APP_WEATHER_API_KEY,
+  NASA_API_BASE_URL: "https://api.nasa.gov/insight_weather",
+  NASA_API_KEY: process.env.REACT_APP_NASA_API_KEY,
+  WEATHERAPI_BASE_URL: "https://api.weatherapi.com/v1",
 };
 
 export default config;
